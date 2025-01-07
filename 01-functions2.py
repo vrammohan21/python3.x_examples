ae63ebca24e6ff4f40e0b2bef3bd3dae9adea584
vrammohan21 <chvrammohan21@gmail.com>
2025-01-07 12:03:25 +0530

    no

--- 01-functions2.py
+++ 01-functions2.py
@@ -3,5 +3,8 @@ def hello(user="no input from user") :
     print("Hello",user)
 hello()
 name=input("what's ur name?")
-hello(name)
+if(len(name)==0):
+    print("Hello User welcome.You did not entered your name")
+else:
+    hello(name)
 #print(name)
