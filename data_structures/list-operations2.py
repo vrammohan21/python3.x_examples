67ce976a98d0b6b7a43ba12a973e020f68b77c31
vrammohan21 <chvrammohan21@gmail.com>
2025-02-12 06:00:55 +0530

    Refactored code.

--- data_structures/list-operations2.py
+++ data_structures/list-operations2.py
@@ -1,41 +1,35 @@
-student_ids=[1,2,3,4]
-#print(type(student_ids))
-print('Original Student IDs:',student_ids,'& Total students:',len(student_ids))
-print("address of original student_ids=",id(student_ids))
-#Adding a new element to the list
-student_ids.append(999)
-print('after appending 999:',student_ids,'&total students:',len(student_ids),id(student_ids))
-student_ids.extend([22,33,44])
-#extend operation: adding MULTIPLE ELEMENTS to student_ids list
-print('after EXTENDING 22,33,44:',student_ids,'&total students:',len(student_ids))
-#get the index of a SPECIFIC ELEMENT.
-print('Index of the element 22:',student_ids.index(22))
+#This example will show all the operations on a list.
+my_list=[1,2,3,4]
+print(type(my_list))
+print(f'Original list: {my_list} ,count: {len(my_list)} & address:{id(my_list)}')
+#append()- Adds an element to the list at the end
+my_list.append(999)
+print(f"after appending 999: {my_list} &count: {len(my_list)}")
+#extend()-adds MULTIPLE ELEMENTS to my_list at the end.
+my_list.extend([22,33,44])
+print(f'after EXTENDING 22,33,44:{my_list} & new list count: {len(my_list)}')
+#index(<ELEMENT>)-get the index of <ELEMENT> if it is in list or Gives "ValueError"
+print(f'Index of the element 22:{my_list.index(22)}')
 #insert() - at a specified index
-student_ids.insert(5,'new')
-print("After insert operation:",student_ids)
-#Removes the first MATCHING element from the list
-#student_ids.remove('new')
-#print("after remove operation:",student_ids)
-print("after remove operation:",student_ids.remove('new'))
-#pop - it removes an element at a specified index, also it returns the removed element.
-print("after pop operation",student_ids.pop(5))
-#reverse method reverses the list
-student_ids.reverse()
-print("after reverse operation:",student_ids)
-#sort method sorts the list in ASCENDING order
-student_ids.sort()
-print("address of student_ids=",id(student_ids))
-print("after sort operation in ASCENDING order:",student_ids)
+my_list.insert(5,'new')
+print(f"After insert operation:{my_list}")
+#remove()-Removes the first MATCHING element from the list do not return any value.
+print(f"after remove operation:{my_list.remove('new')}& current list={my_list}")
+#pop() - removes an element at a specified index & returns the removed element.
+print(f"after pop operation {my_list.pop(5)} & {my_list}")
+#reverse()- reverses the list 
+my_list.reverse()
+print(f"after reverse operation:{my_list}")
+#sort()- sorts the list : default order = ASCENDING
+my_list.sort()
+print(f"after sort operation in ASCENDING order:{my_list}")
 #To sort in descending order
-#student_ids.sort(reverse=True)
-student_ids.reverse()
-print("after sort operatin in DESCENDING order:",student_ids)
+my_list.sort(reverse=True)
+print(f"after sort operatin in DESCENDING order:{my_list}")
+#print("address of my_list =",id(my_list))
 #copy returns a shallow copy of the original list meaning that it creates a new object&returns
-student_ids1=student_ids.copy()
-print("address of student_ids,student_ids1 lists:",id(student_ids),id(student_ids1))
-
-
-
-#KEEP IT AT THE END - clearing the list
-#student_ids.clear()
-#print("after clear() operation:",student_ids,'total count:',len(student_ids))
+student_ids1=my_list.copy()
+print(f"address of my_list {id(my_list)} & student_ids1 {id(student_ids1)}")
+#clear() - removes all the elements from a list.
+my_list.clear()
+print(f"after clear() operation:{my_list} & total count:{len(my_list)} & address {id(my_list)}")
